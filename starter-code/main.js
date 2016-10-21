8be7c5abbb1248952d8c30f93853f14fe48efe82
kfili <kosta.filiotis@gmail.com>
2016-10-20 22:40:34 -0400

    cards are now created through JS rather than HTML
    the divs representing cards were removed from the html file
    and are now created by the JS code.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,27 @@
 'use strict';
 
-console.log("JS file is connected to HTML! Woo!");
+// console.log("JS file is connected to HTML! Woo!");
 
 var cardOne = 'queen';
 var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
-if (cardTwo === cardFour) {
-  window.alert('You found a match!');
-} else {
-  window.alert('Sorry, try again.');
+// if (cardTwo === cardFour) {
+//   window.alert('You found a match!');
+// } else {
+//   window.alert('Sorry, try again.');
+// }
+
+
+
+
+
+function createCards() {
+var gameBoard = document.getElementById("game-board");
+  for (var i = 0; i <= 4; i++) {
+    var card = document.createElement("div");
+    card.className = "card";
+    gameBoard.appendChild(card);
+  }
 }
