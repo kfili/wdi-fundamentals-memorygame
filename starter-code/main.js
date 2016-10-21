68e03999901602aaf49d594edc6bd2c49c904773
kfili <kosta.filiotis@gmail.com>
2016-10-21 07:59:07 -0400

    Final part of fundamentals
    attempted to complete unit 11. I ran out of time,
    and fundamentals stopped loading in my browser.

--- starter-code/main.js
+++ starter-code/main.js
@@ -13,15 +13,49 @@ var cardFour = 'king';
 //   window.alert('Sorry, try again.');
 // }
 
+var cards = ['queen', 'queen', 'king', 'king'];
 
+var cardsInPlay = [];
 
-
+function createBoard() {
+  var board = document.getElementByClassNAme('board')[0];
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.InnerHTML = cards[i];
+    cardElement.addEventListener('click', isTwoCards);
+    cardElement.InnerHTML = '<img src="KingImg.jpg" alt="King" />';
+    board.appendChild(cardElement);
+  }
+}
 
 function createCards() {
-var gameBoard = document.getElementById("game-board");
+var Board = document.getElementById("game-board");
   for (var i = 0; i <= 4; i++) {
     var card = document.createElement("div");
     card.className = "card";
     gameBoard.appendChild(card);
   }
 }
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('card'));
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+if (cards[0] === cards[1]) {
+  alert('Its a match')
+  }
+  else {
+  alert('try again!')
+}
+}
+
+// for (var i=0; i<cards.length; i++) {
+//     cardElement.addEventListener('click', isTwoCards);
+//     cardElement.InnerHTML = '<img src="my_king.png" alt="King of Spades" />';
+// }
